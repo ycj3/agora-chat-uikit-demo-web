b1922eceec0c7e7151271051d029989b6e14e368
CarlsonYuan <carlsonyuandev@gmail.com>
2024-10-23 15:48:24 +0800

    initialize

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+// App.js
+import React, { Component, useEffect } from "react";
+import {
+  Provider,
+  Chat,
+  ConversationList,
+  useClient,
+  rootStore,
+} from "agora-chat-uikit";
+import "agora-chat-uikit/style.css";
 
-function App() {
+const appKey = "you app key"; // your appKey
+const user = ""; // your user ID
+const agoraToken = ""; // agora chat token
+
+const conversation = {
+  chatType: "singleChat", // 'singleChat' || 'groupChat'
+  conversationId: "agora", // target user id or group id
+  name: "Agora", // target user nickname or group name
+  lastMessage: {},
+};
+
+const ChatApp = () => {
+  const client = useClient();
+  useEffect(() => {
+    client &&
+      client
+        .open({
+          user,
+          agoraToken,
+        })
+        .then((res) => {
+          console.log("get token success", res);
+          // create a conversation
+          rootStore.conversationStore.addConversation(conversation);
+        });
+  }, [client]);
+
+  client.addEventHandler("chatdemo", {
+    onConnected: () => {
+      rootStore.conversationStore.setCurrentCvs(conversation)
+    }}) 
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>
+        <ConversationList />
+      </div>
+      <div>
+        <Chat />
+      </div>
     </div>
   );
+};
+
+class App extends Component {
+  render() {
+    return (
+      <Provider
+        initConfig={{
+          appKey,
+        }}
+      >
+        <ChatApp />
+      </Provider>
+    );
+  }
 }
 
 export default App;
